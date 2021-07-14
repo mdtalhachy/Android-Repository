7ba793ec01d4abe9f6edbc2364bf3e5ebd1b7e3c
MD Talha Chowdhury <talhachowdhury3.1416@gmail.com>
2021-07-14 16:59:46 +0600

    Fetching JSON data as JSONObjects

--- JsonParsing/MainActivity.java
+++ JsonParsing/MainActivity.java
@@ -10,20 +10,52 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 
+    RequestQueue queue;
     String url = "https://www.google.com";
+    String apiUrl = "https://jsonplaceholder.typicode.com/todos";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RequestQueue queue = Volley.newRequestQueue(this);
+        queue = Volley.newRequestQueue(this);
 
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, apiUrl, null,
+                response -> {
+
+                    for (int i = 0; i < response.length(); i++) {
+
+                        try {
+                            JSONObject jsonObject = response.getJSONObject(i);
+                            Log.d("JSON", "onCreate: " + jsonObject.getString("title"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                }, error -> {
+                    Log.d("JSON", "onCreate: Failed");
+                });
+
+
+        queue.add(jsonArrayRequest);
+
+        getString(queue);
+    }
+
+    @org.jetbrains.annotations.NotNull
+    private StringRequest getString(RequestQueue queue) {
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url, response -> {
 
             //displaying contents of our URL
@@ -34,5 +66,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         queue.add(stringRequest);
+
+        return stringRequest;
     }
 }
