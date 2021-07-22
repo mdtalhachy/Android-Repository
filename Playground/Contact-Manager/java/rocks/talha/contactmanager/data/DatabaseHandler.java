5c56c8efd5f2d60cd3fba4ba774c91d24ce72318
MD Talha Chowdhury <talhachowdhury3.1416@gmail.com>
2021-07-22 22:30:35 +0600

    Added updateContact()

--- Playground/Contact-Manager/java/rocks/talha/contactmanager/data/DatabaseHandler.java
+++ Playground/Contact-Manager/java/rocks/talha/contactmanager/data/DatabaseHandler.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.view.contentcapture.ContentCaptureCondition;
 
 import androidx.annotation.Nullable;
 
@@ -102,5 +103,19 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         }
 
         return contactList;
+    }
+
+    //update contact
+    public int updateContact(Contact contact){
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(Util.KEY_NAME, contact.getName());
+        values.put(Util.KEY_PHONE_NUMBER, contact.getPhoneNumber());
+
+        //update the row
+        //whenever the system updates something, it returns a int value
+        return db.update(Util.TABLE_NAME, values, Util.KEY_ID + "=?",
+                new String[]{String.valueOf(contact.getId())});
     }
 }
