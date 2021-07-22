62f7949d1f7ed00aa68a36df67aa463ea8999140
MD Talha Chowdhury <talhachowdhury3.1416@gmail.com>
2021-07-22 13:23:55 +0600

    addContact and getContact using SQL CRUD

--- Playground/Contact-Manager/java/rocks/talha/contactmanager/data/DatabaseHandler.java
+++ Playground/Contact-Manager/java/rocks/talha/contactmanager/data/DatabaseHandler.java
@@ -1,12 +1,15 @@
 package rocks.talha.contactmanager.data;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
 import rocks.talha.contactmanager.R;
+import rocks.talha.contactmanager.model.Contact;
 import rocks.talha.contactmanager.util.Util;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
@@ -19,7 +22,7 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
 
         String CREATE_CONTACT_TABLE = "CREATE TABLE " + Util.TABLE_NAME + "("
-                + Util.KEY_ID + " INTEGER PRIMARY KEY" + Util.KEY_NAME + " TEXT"
+                + Util.KEY_ID + " INTEGER PRIMARY KEY," + Util.KEY_NAME + " TEXT,"
                 + Util.KEY_PHONE_NUMBER + " TEXT" + ")";
 
         db.execSQL(CREATE_CONTACT_TABLE); //executing our SQL
@@ -34,6 +37,42 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         db.execSQL(DROP_TABLE, new String[]{Util.TABLE_NAME});
 
         onCreate(db); //recreating table once dropped
-        
+
+    }
+
+    /* CRUD - Create, Read, Update, Delete
+     */
+
+    //Add contact
+    public void addContact(Contact contact){
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(Util.KEY_NAME, contact.getName());
+        values.put(Util.KEY_PHONE_NUMBER, contact.getPhoneNumber());
+
+        //insert to row
+        db.insert(Util.TABLE_NAME, null, values);
+        db.close(); //closing db connection
+    }
+
+    //Get a contact
+    public Contact getContact(int id){
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(Util.TABLE_NAME,
+                new String[]{Util.KEY_ID, Util.KEY_NAME, Util.KEY_PHONE_NUMBER},
+                Util.KEY_ID +"=?", new String[]{String.valueOf(id)},
+                null, null, null);
+
+        if(cursor != null)
+            cursor.moveToFirst();
+
+        Contact contact = new Contact();
+        contact.setId(Integer.parseInt((cursor.getString(0))));
+        contact.setName(cursor.getString(1));
+        contact.setPhoneNumber(cursor.getString(2));
+
+        return contact;
     }
 }
