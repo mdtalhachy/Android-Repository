ca69f55c12f81b4d84e1605d24d293e9426d4f99
MD Talha Chowdhury <talhachowdhury3.1416@gmail.com>
2021-07-23 21:40:04 +0600

    Added model Contact Class

---
+++ Playground/ContactRoom/java/rocks/talha/contactroom/model/Contact.java
@@ -0,0 +1,40 @@
+package rocks.talha.contactroom.model;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "contact_table")
+public class Contact {
+
+    @PrimaryKey (autoGenerate = true)
+    @ColumnInfo (name = "id")
+    private int id;
+
+    @ColumnInfo (name = "name")
+    private String name;
+
+    @ColumnInfo (name = "occupation")
+    private String occupation;
+
+    public Contact() {
+    }
+
+    public Contact(@NonNull String name, @NonNull String occupation) {
+        this.name = name;
+        this.occupation = occupation;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+}
