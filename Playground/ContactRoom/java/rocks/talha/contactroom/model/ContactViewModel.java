a1d829dbc56522e83f928791ec8dae62d6942447
MD Talha Chowdhury <talhachowdhury3.1416@gmail.com>
2021-07-26 16:20:34 +0600

    Create ContactViewModel.java

---
+++ Playground/ContactRoom/java/rocks/talha/contactroom/model/ContactViewModel.java
@@ -0,0 +1,32 @@
+package rocks.talha.contactroom.model;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+import rocks.talha.contactroom.data.ContactRepository;
+
+public class ContactViewModel extends AndroidViewModel {
+
+    public static ContactRepository repository;
+    public final LiveData<List<Contact>> allContacts;
+
+    public ContactViewModel(@NonNull Application application) {
+        super(application);
+
+        repository = new ContactRepository(application);
+        allContacts = repository.getAllData();
+    }
+
+    public LiveData<List<Contact>> getAllContacts(){
+        return allContacts;
+    }
+
+    public static void insert(Contact contact){
+        repository.insert(contact);
+    }
+}
