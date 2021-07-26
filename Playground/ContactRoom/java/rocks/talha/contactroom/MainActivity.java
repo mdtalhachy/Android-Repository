1e0e22e836cbe4c997e1147db9bbd86cf1b24e73
MD Talha Chowdhury <talhachowdhury3.1416@gmail.com>
2021-07-26 16:48:00 +0600

    Testing ContactViewModel

--- Playground/ContactRoom/java/rocks/talha/contactroom/MainActivity.java
+++ Playground/ContactRoom/java/rocks/talha/contactroom/MainActivity.java
@@ -1,14 +1,38 @@
 package rocks.talha.contactroom;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
+
+import rocks.talha.contactroom.model.Contact;
+import rocks.talha.contactroom.model.ContactViewModel;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ContactViewModel contactViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        contactViewModel = new ViewModelProvider.AndroidViewModelFactory(MainActivity.this
+                .getApplication())
+                .create(ContactViewModel.class);
+
+
+        contactViewModel.getAllContacts().observe(this, new Observer<List<Contact>>() {
+            @Override
+            public void onChanged(List<Contact> contacts) {
+                for(Contact contact : contacts){
+                    Log.d("CTEST", "onChanged: " + contact.getName());
+                }
+            }
+        });
     }
 }
