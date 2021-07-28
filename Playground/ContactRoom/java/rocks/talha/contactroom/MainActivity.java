76f5f0755e3f98d99c5e4a807dfa5d059aa76a5d
Talha Chowdhury <talhachowdhury3.1416@gmail.com>
2021-07-28 16:18:40 +0600

    Added floating action button functionalities
    Passed data from NewContact to MainActivity through intent. If we click fab, the activity will go to new Contact and once we click save, the activity will come back to MainActivity.

--- Playground/ContactRoom/java/rocks/talha/contactroom/MainActivity.java
+++ Playground/ContactRoom/java/rocks/talha/contactroom/MainActivity.java
@@ -1,14 +1,19 @@
 package rocks.talha.contactroom;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
 
@@ -17,7 +22,9 @@ import rocks.talha.contactroom.model.ContactViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int NEW_CONTACT_ACTIViTY_REQ_CODE = 1;
     private ContactViewModel contactViewModel;
+    TextView textView;
 
 
     @Override
@@ -25,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textView = findViewById(R.id.textView);
 
         contactViewModel = new ViewModelProvider.AndroidViewModelFactory(MainActivity.this
                 .getApplication())
@@ -41,7 +49,32 @@ public class MainActivity extends AppCompatActivity {
                     Log.d("CTEST", "onChanged: " + contact.getName());
                 }
 
+                textView.setText(builder);
             }
         });
+
+        FloatingActionButton floab = findViewById(R.id.add_contact_fab);
+
+        floab.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, NewContact.class);
+            startActivityForResult(intent, NEW_CONTACT_ACTIViTY_REQ_CODE);
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == NEW_CONTACT_ACTIViTY_REQ_CODE && resultCode == RESULT_OK){
+            /*Log.d("Bull", "onActivityResult: " + data.getStringExtra(NewContact.NAME_KEY));
+            Log.d("Bulls", "onActivityResult: " + data.getStringExtra(NewContact.OCCU_KEY));*/
+
+            String name = data.getStringExtra(NewContact.NAME_KEY);
+            String occupation = data.getStringExtra(NewContact.OCCU_KEY);
+            Contact contact = new Contact(name, occupation);
+
+            ContactViewModel.insert(contact);
+        }
+
     }
 }
