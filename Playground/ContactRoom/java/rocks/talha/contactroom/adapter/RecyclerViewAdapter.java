e02e4d1399e42a1eb028d26b83661b82a9d24346
Talha Chowdhury <talhachowdhury3.1416@gmail.com>
2021-07-29 18:17:34 +0600

    Update RecyclerViewAdapter.java

--- Playground/ContactRoom/java/rocks/talha/contactroom/adapter/RecyclerViewAdapter.java
+++ Playground/ContactRoom/java/rocks/talha/contactroom/adapter/RecyclerViewAdapter.java
@@ -1,1 +1,65 @@
 
+package rocks.talha.contactroom.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+import rocks.talha.contactroom.R;
+import rocks.talha.contactroom.model.Contact;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    private List<Contact> contactList;
+    private Context context;
+
+    public RecyclerViewAdapter(List<Contact> contactList, Context context) {
+        this.contactList = contactList;
+        this.context = context;
+    }
+
+    @NonNull
+    @org.jetbrains.annotations.NotNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull @org.jetbrains.annotations.NotNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(context)
+                .inflate(R.layout.contact_row, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @org.jetbrains.annotations.NotNull RecyclerViewAdapter.ViewHolder holder, int position) {
+        Contact contact = contactList.get(position);
+        holder.name.setText(contact.getName());
+        holder.occupation.setText(contact.getOccupation());
+    }
+
+    @Override
+    public int getItemCount() {
+        return contactList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView name;
+        public TextView occupation;
+
+        public ViewHolder(@NonNull @NotNull View itemView) {
+            super(itemView);
+
+            name = (TextView) itemView.findViewById(R.id.name_textView);
+            occupation = (TextView) itemView.findViewById(R.id.occupation_textView);
+        }
+    }
+}
