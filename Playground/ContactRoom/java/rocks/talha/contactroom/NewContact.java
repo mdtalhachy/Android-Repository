64dcf8de45ab9e71a8f937b7aa36af75e92db147
Talha Chowdhury <talhachowdhury3.1416@gmail.com>
2021-07-31 06:12:49 +0600

    Added Update & Delete Button Functionalities.

--- Playground/ContactRoom/java/rocks/talha/contactroom/NewContact.java
+++ Playground/ContactRoom/java/rocks/talha/contactroom/NewContact.java
@@ -1,6 +1,7 @@
 package rocks.talha.contactroom;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
@@ -10,6 +11,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import rocks.talha.contactroom.model.Contact;
 import rocks.talha.contactroom.model.ContactViewModel;
@@ -21,6 +24,10 @@ public class NewContact extends AppCompatActivity {
     private EditText enterName;
     private EditText enterOccupation;
     private Button saveButton;
+    private Button updateButton;
+    private Button deleteButton;
+    private int contactId = 0;
+    private boolean isEdit = false;
 
     private ContactViewModel contactViewModel;
 
@@ -32,6 +39,18 @@ public class NewContact extends AppCompatActivity {
         contactViewModel = new ViewModelProvider.AndroidViewModelFactory(NewContact.this
                 .getApplication())
                 .create(ContactViewModel.class);
+
+        if(getIntent().hasExtra("contact_id")){
+            contactId = getIntent().getIntExtra("contact_id", 0);
+
+            contactViewModel.get(contactId).observe(this, contact -> {
+                if(contact != null){
+                    enterName.setText(contact.getName());
+                    enterOccupation.setText(contact.getOccupation());
+                }
+            });
+            isEdit = true;
+        }
 
         enterName = findViewById(R.id.name_edit_text);
         enterOccupation = findViewById(R.id.occupation_edit_text);
@@ -56,5 +75,44 @@ public class NewContact extends AppCompatActivity {
 
             finish();
         });
+
+
+        //Update Button
+        updateButton = findViewById(R.id.update_button);
+        deleteButton = findViewById(R.id.delete_button);
+
+        updateButton.setOnClickListener(view -> {
+            int id = contactId;
+            String name = enterName.getText().toString().trim();
+            String occupation = enterOccupation.getText().toString().trim();
+
+            if(TextUtils.isEmpty(name) || TextUtils.isEmpty(occupation)){
+                Snackbar.make(enterName, "Empty Fields", Snackbar.LENGTH_SHORT).show();
+            }else{
+                Contact contact = new Contact();
+                contact.setId(id);
+                contact.setName(name);
+                contact.setOccupation(occupation);
+                ContactViewModel.update(contact);
+                finish();
+            }
+        });
+
+        //Delete Button Functionality
+        deleteButton.setOnClickListener(view -> {
+            int id = contactId;
+            Contact contact = new Contact();
+            contact.setId(contactId);
+            ContactViewModel.delete(contact);
+            finish();
+        });
+
+        if(isEdit){
+            saveButton.setVisibility(View.GONE);
+        }else{
+            updateButton.setVisibility(View.GONE);
+            deleteButton.setVisibility(View.GONE);
+        }
+
     }
 }
