5efbd0e89be69f5f174bcc8fe24556366eff16dd
Talha Chowdhury <talhachowdhury3.1416@gmail.com>
2021-07-31 06:14:47 +0600

    Added Update & Delete Buttons Funcationalities.
    Added empty constructor.

--- Playground/ContactRoom/java/rocks/talha/contactroom/model/Contact.java
+++ Playground/ContactRoom/java/rocks/talha/contactroom/model/Contact.java
@@ -17,6 +17,9 @@ public class Contact {
     @ColumnInfo (name = "occupation")
     private String occupation;
 
+    public Contact() {
+    }
+
     public Contact(@NonNull String name, @NonNull String occupation) {
         this.name = name;
         this.occupation = occupation;
