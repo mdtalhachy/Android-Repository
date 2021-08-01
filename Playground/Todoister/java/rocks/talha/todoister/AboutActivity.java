3b4a654862c9cc387f79ce01d42eb689f11b1849
Talha Chowdhury <talhachowdhury3.1416@gmail.com>
2021-08-01 06:29:43 +0600

    Added primary layouts and code.
    Changed package name. Reference: https://stackoverflow.com/questions/16804093/rename-package-in-android-studio

---
+++ Playground/Todoister/java/rocks/talha/todoister/AboutActivity.java
@@ -0,0 +1,16 @@
+package rocks.talha.todoister;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import rocks.talha.todoister.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
