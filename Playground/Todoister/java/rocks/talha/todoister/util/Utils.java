7548561c28bc9d3289cbf26a4ec62a754a5f237d
Talha Chowdhury <talhachowdhury3.1416@gmail.com>
2021-08-05 22:35:15 +0600

    Added Utils.java

---
+++ Playground/Todoister/java/rocks/talha/todoister/util/Utils.java
@@ -0,0 +1,12 @@
+package rocks.talha.todoister.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Utils {
+    public static String formatDate(Date date){
+        SimpleDateFormat simpleDateFormat = (SimpleDateFormat) SimpleDateFormat.getDateInstance();
+        simpleDateFormat.applyPattern("EEE, MMM d");
+        return simpleDateFormat.format(date);
+    }
+}
