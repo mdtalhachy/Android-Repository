ec5127bc4f373e49765f0af3fc165387bf94840b
Talha Chowdhury <talhachowdhury3.1416@gmail.com>
2021-08-09 04:43:09 +0600

    Deleting adapterPosition parameter.

--- Playground/Todoister/java/rocks/talha/todoister/adapter/OnTodoClickListener.java
+++ Playground/Todoister/java/rocks/talha/todoister/adapter/OnTodoClickListener.java
@@ -3,6 +3,6 @@ package rocks.talha.todoister.adapter;
 import rocks.talha.todoister.model.Task;
 
 public interface OnTodoClickListener {
-    void onTodoClick(int adapterPosition, Task task);
+    void onTodoClick(Task task);
     void onTodoRadioButtonClick(Task task);
 }
